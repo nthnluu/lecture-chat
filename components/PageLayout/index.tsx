bf5b3e21d6fcd18acc2bc94c422e8174b5a9d474
nathanluu <nate1299@me.com>
2020-11-14 10:25:45 -0800

    Fixed mobile layout bugs

--- components/PageLayout/index.tsx
+++ components/PageLayout/index.tsx
@@ -70,7 +70,7 @@ const PageLayout: React.FC<Props> = ({
         </Head>
         <LinearProgress hidden={!loading}/>
         <ScrollContext.Provider value={scrollToBottom}>
-            <div className="flex h-screen">
+            <div className="flex h-screen fill-height">
                 <div>
                     <div className="hidden lg:flex h-full">
                         <Sidebar loading={loading} config={sidebarConfig} isOpen={true} mobile={false}/>
